fe255a25b53d503b19670d682781456eafe92245
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-03-08 17:01:14 +0900

    User management
    Added controllers for users & items
    Validation for users params
    gem Bootstrap-datepicker-rails& setup (実験用)
    User registration, login, logout, enabled error messaging
    Added card.js & linked to application.js (preparation)
    Enabled helpers on Rspec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permitted_params, if: :devise_controller?
+  # before_action :authenticate_user!
+  before_action :find_user
+
+  def find_user
+    @user = current_user
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  private
+  def config_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :name, :surname_furigana, :name_furigana, :date_of_birth])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true, length: { maximum: 10 }
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  with_options presence: true do
+    validates :nickname, format: {with: /\A[a-z\d]+\z/i, message: "Is invalid. Alphabets and Numbers only."}
+    validates :surname, format: {with: /\A[ァ-ヶ一-龥々]/, message: "Is invalid. Whole case Kanji/Katakana only."}
+    validates :name, format: {with: /\A[ァ-ヶ一-龥々]/, message: "Is invalid. Whole case Kanji/Katakana only."}
+    validates :surname_furigana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Is invalid. While case Katakana only."}
+    validates :name_furigana, format: {with: /\A[ァ-ヶー－]+\z/, message: "Is invalid. Whole case Katakana only."}
+  end 
 
   has_many :items
   has_many :comments, dependent: :destroy
