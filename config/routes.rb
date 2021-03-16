9044a70876743eee3a38de3a462b2ee16e8a0a4b
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-03-16 14:12:56 +0900

    Possible Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  resources :items, except: [:index] do
+  resources :items do
     resources :orders
     collection do
       get 'search'
