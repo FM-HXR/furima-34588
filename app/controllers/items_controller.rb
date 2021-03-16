2a1a2b77fe82064ff2dce04d9e466f20d9105241
FM-HXR <78189593+FM-HXR@users.noreply.github.com>
2021-03-16 17:02:38 +0900

    Fix 4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :search]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   
   def index
     @items = Item.includes(:user, :order).order("created_at DESC")
